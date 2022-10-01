23fb775a02f03a51e1c6e356b01c425e96134749
Abubakar <gwabstech@gmail.com>
2022-10-01 12:57:43 +0100

    report added

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+print(money_machine.report())
+print(coffee_maker.report())
